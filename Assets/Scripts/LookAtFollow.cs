b72af253d58e46d70d76fa6e8917ba02c08f30d8
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-04-29 10:29:05 -0700

    Added a script for enemies to follow the player

---
+++ Assets/Scripts/LookAtFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform enemyTarget;
+    private float enemySpeed = 5.0f;
+
+    private CollisionTracker collisionTrackerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionTrackerScript = GameObject.Find("Player").GetComponent<CollisionTracker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(collisionTrackerScript.gameOver == false)
+        {
+            transform.LookAt(enemyTarget.position);
+            transform.Translate(0.0f, 0.0f, enemySpeed * Time.deltaTime);
+        }
+        
+    }
+}
