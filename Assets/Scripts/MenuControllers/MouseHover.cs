f38ba4052a5475a0fbd1c8e5cbdc15ad3b4ee32b
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-05-09 17:03:18 -0700

    Added GameOver menu

--- Assets/Scripts/MenuControllers/MouseHover.cs
+++ Assets/Scripts/MenuControllers/MouseHover.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StarMenuMouseHover : MonoBehaviour
+public class MouseHover : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
